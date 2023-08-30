eeeb1a54f55159c47eebe995602dfb7a529e5f3a
unknown <143607652+SH4ZH4M@users.noreply.github.com>
2023-08-30 18:50:54 +0530

    Hemlo

---
+++ HelloW.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string name ="";
+    cin >> name; 
+    cout << "Hello World" << " " << name << "\n";
+    cout << "Hemlo";
+    return 0;
+    
+}
